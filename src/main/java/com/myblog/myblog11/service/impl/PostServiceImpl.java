fe4491f342ceecdbd760742209040e559a1d0873
LenS340(FPIN) <reddytbachi@gmail.com>
2024-02-12 21:36:04 +0530

    Id Added to Page

--- src/main/java/com/myblog/myblog11/service/impl/PostServiceImpl.java
+++ src/main/java/com/myblog/myblog11/service/impl/PostServiceImpl.java
@@ -1,6 +1,7 @@
 package com.myblog.myblog11.service.impl;
 
 import com.myblog.myblog11.Entity.Post;
+import com.myblog.myblog11.exception.ResourseNotFoundException;
 import com.myblog.myblog11.payload.PostDto;
 import com.myblog.myblog11.repository.PostRepository;
 import com.myblog.myblog11.service.PostService;
@@ -9,24 +10,46 @@ import org.springframework.stereotype.Service;
 @Service
 public class PostServiceImpl implements PostService {
 
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
 
     public PostServiceImpl(PostRepository postRepository) {
+
         this.postRepository = postRepository;
     }
 
     @Override
     public PostDto createPost(PostDto postDto) {
-        Post post=new Post();
+        Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
+
         Post savedPost = postRepository.save(post);
 
-        PostDto dto=new PostDto();
-        dto.setTitle(savedPost.getTitle());
-        dto.setDescription(savedPost.getDescription ());
-        dto.setContent(savedPost.getContent());
+
+        postDto.setId(savedPost.getId());
+        postDto.setTitle(savedPost.getTitle());
+        postDto.setDescription(savedPost.getDescription());
+        postDto.setContent(savedPost.getContent());
+
+        return postDto;
+    }
+
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(
+                ()-> new ResourseNotFoundException("Post not Found with id" + id)
+        );
+
+        PostDto dto = new PostDto();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setDescription(post.getDescription ());
+        dto.setContent(post.getContent());
+
         return dto;
     }
+
+
 }
